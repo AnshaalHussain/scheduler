bf250b225a07cdda5534a4314c3566775851eb25
Anshaal Hussain <anshaalhussain@gmail.com>
2021-11-06 05:13:11 -0400

    Combine components into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,6 +22,45 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+  },
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   return (
@@ -35,11 +75,7 @@ export default function Application(props) {
             />
             <hr className="sidebar__separator sidebar--centered" />
             <nav className="sidebar__menu">
-              <DayList
-                days={days}
-                value={day}
-                onChange={setDay}
-              />
+              <DayList days={days} value={day} onChange={setDay} />
             </nav>
             <img
               className="sidebar__lhl sidebar--centered"
@@ -50,7 +86,9 @@ export default function Application(props) {
         }
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((x) => (
+          <Appointment keys={x.id} {...x}  />
+        ))}
       </section>
     </main>
   );
